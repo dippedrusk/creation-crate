d151a3dedded13342229c27a2351f3aabdb70a18
vgautam <vasundhara1997@gmail.com>
2021-01-09 17:15:06 -0800

    p1 mood lamp minor tweaks

--- p1-mood-lamp/notes.md
+++ p1-mood-lamp/notes.md
@@ -3,9 +3,13 @@
 * There are lot of complicated ways to transmit data and instructions. Need to rewatch that section and do a Wikipedia deep dive.
 * U-shaped jumper cables! Like regular jumper cables, but flat!
 * The analogue version of `digitalWrite` is... `analogWrite`!
+* Not a fan of the sudden change from 100% brightness to 0%. Rather than having the delay be inversely proportional to the brightness, I want it inversely proportional to the magnitude away from the "middle" - whether light or dark.
 
 ## Questions
 
 1. How does the LDR actually work? Why do we hook the "positive side" of the LDR to the analogue input?
 My guess is that we get a current reading from that part and it changes depending on how much resistance the LDR has, which in turn depends on the ambient light.
 But why wouldn't we hook it up at the other end of the LDR when the current has gone through?
+2. What's the best way to debug Arduino code at runtime without using the IDE? What's the Arduino equivalent of console.log?
+3. Why does `power` have to be between 2 and 255?
+It's to do with [analogWrite](https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/) - you need to give it a value between 0 (off) and 255 (on). Not a concidence that this feels binaryish.
--- p1-mood-lamp/p1-mood-lamp.ino
+++ p1-mood-lamp/p1-mood-lamp.ino
@@ -2,52 +2,59 @@ int redLED = 11;
 int greenLED = 10;
 int blueLED = 9;
 int LDR = 0;
+float x = 0.0; // name this better
+float x_increment = 0.0001; // also name this better
 
-unsigned int pulseSpeed = 5;
-int power = 150; // why? (their comment says 2-255? also why?)
-float rad_multiplier = 180/PI;
+unsigned int pulseDelay = 1;
+int power = 25;
+float radMultiplier = 180/PI;
+// int minDarkness = 900; // prod - calibrated to my room
+int minDarkness = 600; // dev
 
 float RGB[3] = {0.0, 0.0, 0.0};
 
 void setup() {
     pinMode(redLED, OUTPUT);
-    pinMode(blueLED, OUTPUT);
     pinMode(greenLED, OUTPUT);
+    pinMode(blueLED, OUTPUT);
 
     digitalWrite(redLED, LOW);
-    digitalWrite(blueLED, LOW);
     digitalWrite(greenLED, LOW);
+    digitalWrite(blueLED, LOW);
 }
 
 void loop() {
-    for (float x = 0; x < PI; x += 0.00001) {
-        // why? this looks vaguely familiar - what math is it? why isn't this simpler?
-        RGB[0] = power * abs(sin(x *rad_multiplier));
-        RGB[1] = power * abs(sin((x + PI/3) * rad_multiplier));
-        RGB[2] = power * abs(sin((x + 2*PI)/3) * rad_multiplier);
-        if (analogRead(LDR) > 600) { // why 600?
-            analogWrite(redLED, RGB[0]);
-            analogWrite(greenLED, RGB[1]);
-            analogWrite(blueLED, RGB[2]);
-        } else {
-            // Lights go on, lamp goes off
-            digitalWrite(redLED, LOW);
-            digitalWrite(blueLED, LOW);
-            digitalWrite(greenLED, LOW);
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (RGB[i] < 1) {
-                delay(20 * pulseSpeed);
-            } else if (RGB[i] < 5) {
-                delay(10 * pulseSpeed);
-            } else if (RGB[i] < 10) {
-                delay(2 * pulseSpeed);
-            } else if (RGB[i] < 100) {
-                delay(pulseSpeed);
-            } // why an empty else? can i simplify this?
-            delay(1);
-        }
+    if ((x >= PI) || (x < 0.0)) {
+        x_increment = 0 - x_increment;
     }
 
+    // why? this looks vaguely familiar - what math is it? why isn't this simpler?
+    RGB[0] = power * abs(sin(x * radMultiplier));
+    RGB[1] = power * abs(sin((x + PI/3) * radMultiplier));
+    RGB[2] = power * abs(sin(x + (2*PI)/3) * radMultiplier);
+    if (analogRead(LDR) > minDarkness) {
+        analogWrite(redLED, RGB[0]);
+        analogWrite(greenLED, RGB[1]);
+        analogWrite(blueLED, RGB[2]);
+    } else {
+        // Lights go on, lamp goes off
+        digitalWrite(redLED, LOW);
+        digitalWrite(greenLED, LOW);
+        digitalWrite(blueLED, LOW);
+    }
+
+    // calculates a delay that's inversely proportional to the LED's current brightness
+    for (int i = 0; i < 3; i++) {
+        if (RGB[i] < 1) {
+            delay(8 * pulseDelay);
+        } else if (RGB[i] < 5) {
+            delay(4 * pulseDelay);
+        } else if (RGB[i] < 10) {
+            delay(2 * pulseDelay);
+        } else if (RGB[i] < 100) {
+            delay(pulseDelay);
+        } // why an empty else? can i simplify this?
+    }
+    delay(1);
+    x += x_increment;
 }
